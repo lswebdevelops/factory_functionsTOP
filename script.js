06104cc5fe1d06ec7467349557eb48dd01bd4b70
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-10-15 19:43:21 -0300

    Game working by click showing x and double click 'o'

--- script.js
+++ script.js
@@ -1,76 +1,166 @@
-// const personFactory = (name, age) => {
-//     const sayHello = () => console.log('hello');
-//     const sayBye =() => 'Bye bye';
-//     const addX = () => 'X';
-//     const addO =() => 'O';
-//     //new way:
-//     return{ name, age, sayHello , sayBye, addX, addO};
-//     // old way: 
-//     //return {name: name, age: age, sayHello: sayHello, sayBye: sayBye};
-// };
+// Create a tic tac toe game
 
-// const jeff = personFactory('jeff', 27);
-
-// // document.getElementById('p1').innerText= jeff.name;
-// // document.getElementById('p2').innerText = jeff.sayBye();
+let showX = document.querySelectorAll('.container div');
+let one = showX[0]
+let two = showX[1]
+let three = showX[2]
+let four = showX[3]
+let five = showX[4]
+let six = showX[5]
+let seven = showX[6]
+let eight = showX[7]
+let nine = showX[8]
 
 
 
-// // adding button x
- 
-// const childP = document.getElementById('h1');
-
-// const childP3 = document.createElement('hr');
-// const childP4 = document.createElement('button');
-
-// childP4.classList.add('btnClass');
-// childP.appendChild(childP3);
-// childP.appendChild(childP4).style.width = "150px";
-// childP4.style.height = '35px';
-// childP4.style.backgroundColor = 'green';
-// childP4.style.color = 'white';
-// childP4.style.border = 'none';
-
-// // adding button o
- 
-// const childPO = document.getElementById('h2');
-// // no hr
-// const childP4O = document.createElement('button');
-
-// childP4O.classList.add('btnOClass');
-// childPO.appendChild(childP4O).style.width = "150px";
-// childP4O.style.height = '35px';
-// childP4O.style.backgroundColor = 'green';
-// childP4O.style.color = 'white';
-// childP4O.style.border = 'none';
+one.addEventListener('click', function change2O(){
+one.style.color = 'blue';
+one.textContent = 'x'
 
 
-// document.querySelector('.btnClass').innerText = "X"
-// document.querySelector('.btnOClass').innerText = "O"
+})
+two.addEventListener('click', function change2O(){
+    two.textContent ='x'
+    two.style.color = 'blue';
+    
+    })
+three.addEventListener('click', function change2O(){
+    three.textContent ='x'
+    three.style.color = 'blue';
+})
+four.addEventListener('click', function change2O(){
+four.style.color = 'blue';
+four.textContent ='x'
+})
+five.addEventListener('click', function change2O(){
+five.style.color = 'blue';
+five.textContent ='x'
 
-// //adding x paragrath
-// let addBtn2 = document.querySelector('.btnClass');
-//  addBtn2.addEventListener('click',function addX(){
-//     const childP = document.getElementById('h1');
-//     const childP3 = document.createElement('p');
-//     childP3.classList.add('testClass');
-//     childP.appendChild(childP3);
-//     document.querySelector('.testClass').innerText = jeff.addX();
-// });
+})
 
-// //adding o paragrath
-// let addBtn2O = document.querySelector('.btnOClass');
-// addBtn2O.addEventListener('click',function addO(){
-//     const childPO = document.getElementById('h2');
-//     const childP3O = document.createElement('p');
-//     childP3O.classList.add('testClassO');
-//     childPO.appendChild(childP3O);
-//     document.querySelector('.testClassO').innerText = jeff.addO();
-// });
+six.addEventListener('click', function change2O(){
+six.style.color = 'blue';
+six.textContent ='x'
 
-// trying to create a tic tac toe game using factory fuctory functions
+})
+seven.addEventListener('click', function change2O(){
+seven.style.color = 'blue';
+seven.textContent ='x'
+})
+eight.addEventListener('click', function change2O(){
+eight.style.color = 'blue';
+eight.textContent ='x'
 
+})
+nine.addEventListener('click', function change2O(){ 
+nine.style.color = 'blue';
+nine.textContent ='x'
+})
 
 
+// avoiding double click selection:
 
 
+one.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+       
+    }
+});
+two.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+    }
+});
+three.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+    }
+});
+four.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+    }
+});
+five.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+    }
+});
+six.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+    }
+});
+seven.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+    }
+});
+eight.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+    }
+});
+nine.addEventListener('mousedown', function(e){
+    if (e.detail > 1){
+        e.preventDefault();
+    }
+});
+
+// adding O to the div with doble click
+    one.addEventListener('dblclick', function change2O(){
+        one.style.color = 'green';
+        one.textContent = 'o'
+    })
+    two.addEventListener('dblclick', function change2O(){
+        two.style.color = 'green';
+        two.textContent ='o'
+    })
+    three.addEventListener('dblclick', function change2O(){
+    three.style.color = 'green';
+    three.textContent ='o'
+    })
+    four.addEventListener('dblclick', function change2O(){
+    four.style.color = 'green';
+    four.textContent ='o'
+    })
+    five.addEventListener('dblclick', function change2O(){
+    five.style.color = 'green';
+    five.textContent ='o'
+    })
+    six.addEventListener('dblclick', function change2O(){
+    six.style.color = 'green';
+    six.textContent ='o'
+    })
+    seven.addEventListener('dblclick', function change2O(){
+    seven.style.color = 'green';
+    seven.textContent ='o'
+    })
+    eight.addEventListener('dblclick', function change2O(){
+    eight.style.color = 'green';
+    eight.textContent ='o'
+    })
+    nine.addEventListener('dblclick', function change2O(){ 
+    nine.style.color = 'green';
+    nine.textContent ='o'
+    })
+    
+
+
+    // let showX = document.querySelectorAll('.container div');
+    // let one = showX[0]
+    // let two = showX[1]
+    // let three = showX[2]
+    // let four = showX[3]
+    // let five = showX[4]
+    // let six = showX[5]
+    // let seven = showX[6]
+    // let eight = showX[7]
+    // let nine = showX[8]
+
+// for(var i=0; i<showX.length;i++){
+//   if(showX[i].textContent === "x"){
+//    console.log(showX[i]);
+//   }
+// }
