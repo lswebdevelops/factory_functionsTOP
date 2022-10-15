23dd757d13dccfe684fb19c29a98e46fa6c809e7
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-10-15 20:31:32 -0300

    console logging variables as objects

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ const personFactory = (name, age) => {
   console.log(jeff.name); // 'jeff'
   
   jeff.sayHello(); // calls the function and logs 'hello!'
+
+//   /*/******** */
+const name = "Maynard";
+const color = "red";
+const number = 34;
+const food = "rice";
+
+console.log(name, color, number, food);
+console.log({name, color, number, food});
+
+// /*************/
+
+console.clear();
+
+let a = 17;
+console.log(a);
+
+const func = x => {
+    let a = x;
+    console.log(a);
+}
+func(99) 
+console.log(a);
