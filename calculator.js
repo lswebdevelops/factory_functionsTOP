016d84791bb17bdabaf754d7175abd31888a750d
lswebdevelops <sardanhaluciano9690@gmail.com>
2022-10-21 11:02:43 -0300

    modules in js: calculator

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const calculator = (() => {
+
+    const add = (a, b) => a + b;
+    const sub = (a, b) => a - b;
+    const mul = (a, b) => a * b;
+    const div = (a, b) => a / b;
+
+    return{
+        add, 
+        sub,
+        mul,
+        div,
+
+    };
+
+})();
+
+console.log(calculator.add(3, 4));
+console.log(calculator.sub(3, 4));
+
+console.log(calculator.mul(3, 4));
+console.log(calculator.div(3, 4));
+
