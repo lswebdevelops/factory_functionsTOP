04670e81306848c59c5a81eb46d149edbb3d6b03
lswebdevelops <sardanhaluciano9690@gmail.com>
2022-10-21 16:15:28 -0300

    creating modules

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
-const calculator = (() => {
+// creating modules> a function wraped in parentheses and called after.
+
+// const nameFunction = (() => { return { }; })() 
+
+
+const calculator = ( () => {
 
     const add = (a, b) => a + b;
     const sub = (a, b) => a - b;
@@ -17,7 +22,5 @@ const calculator = (() => {
 
 console.log(calculator.add(3, 4));
 console.log(calculator.sub(3, 4));
-
 console.log(calculator.mul(3, 4));
 console.log(calculator.div(3, 4));
-
