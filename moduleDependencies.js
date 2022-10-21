8862abf6a41a8f5bf323dfa77a04411d51d36f8d
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-10-21 20:53:59 -0300

    modules using the DOM

---
+++ moduleDependencies.js
@@ -0,0 +1,29 @@
+const Formatter = (function() {
+    let yearNow = new Date();
+    const log = (message) => console.log(`[${yearNow.getFullYear()}_${yearNow.getMonth()}_${yearNow.getDay()}] Logger: ${message}`);
+  
+    const makeUppercase = (text) => {
+      log("Making uppercase");
+      return text.toUpperCase();
+    };
+  
+    const writeToDOM = (selector, message) => {
+      document.querySelector(selector).innerHTML = message;
+    }
+    const makeLowerCase = (text) => {
+        log("Making uppercase");
+        return text.toLowerCase();
+      };
+    
+
+    return {
+      makeUppercase,
+      writeToDOM,
+      makeLowerCase,
+    }
+  })();
+  
+  Formatter.writeToDOM(".target", "Hi there");
+  Formatter.writeToDOM(".target2", Formatter.makeLowerCase("small case"));
+
+
--- modulesPrimitives.js
+++ modulesPrimitives.js
@@ -1,15 +1,38 @@
-const Formatter = (function() {
-    let timesRun = 0;
+// const Formatter = (function() {
+//     let timesRun = 0;
   
-    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-    const setTimesRun = () => { 
-      log("Setting times run");
-      ++timesRun;
-    }
+//     const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+//     const setTimesRun = () => { 
+//       log("Setting times run");
+//       ++timesRun;
+//     }
+  
+//     const makeUppercase = (text) => {
+//       log("Making uppercase");
+//       setTimesRun();
+//       return text.toUpperCase();
+//     };
+  
+//     return {
+//       makeUppercase,
+//       timesRun,
+//     }
+//   })();
+
+//   console.log(Formatter.makeUppercase('to upper case'));
+//   console.log(Formatter.timesRun);
+
+// Formatter.timesRun = 133;
+//   console.log(Formatter.timesRun);
+
+const Formatter = (function() {
+    let yearNow = new Date();
+    const log = (message) => console.log(`[${yearNow.getFullYear()}_${yearNow.getMonth()}_${yearNow.getDay()}] Logger: ${message}`);
+    const timesRun = ["\nthat was the first"];
   
     const makeUppercase = (text) => {
       log("Making uppercase");
-      setTimesRun();
+      timesRun.push("\none more added later");
       return text.toUpperCase();
     };
   
@@ -18,9 +41,15 @@ const Formatter = (function() {
       timesRun,
     }
   })();
+  
+  console.log(Formatter.makeUppercase("to upper case"));
+  console.log(Formatter.makeUppercase("to upper case"));
+  console.log(Formatter.makeUppercase("to upper case"));
+  console.log(`Variable "timesRun" length: ${Formatter.timesRun.length}`);
+  console.log(Formatter.timesRun[0]);
+  console.log(Formatter.timesRun[1]);
+  console.log(Formatter.timesRun[2]);
+  console.log(Formatter.timesRun[3]);
 
-  console.log(Formatter.makeUppercase('to upper case'));
-  console.log(Formatter.timesRun);
 
-Formatter.timesRun = 133;
-  console.log(Formatter.timesRun);
+
